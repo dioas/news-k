5ff415dee5b7a318379de0819d4fbc6f7f15d4c8
Gratcy Palma <gratcypalma@gmail.com>
2018-12-19 14:57:02 +0700

    fixed unit test and change deployment ip server

--- test/test.js
+++ test/test.js
@@ -33,7 +33,7 @@ describe('News Scenario', () => {
     id: 22,
     title: 'Pilpres 2019',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    topic: ['Pilpres 2019', 'Jokowi-Amin', 'Prabowo-Sandi'],
+    topic: ['Pilpres 2019', 'Jokowi-Ma\'ruf Amin', 'Prabowo-Sandi'],
     status: 'publish'
   }
 
